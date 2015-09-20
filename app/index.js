fd475eacad85024eabd79fe00a479dd49346fc6c
Matt Travi <programmer@travi.org>
2015-09-19 23:51:47 -0500

    reformatted to increase the indentation

--- app/index.js
+++ app/index.js
@@ -3,68 +3,68 @@ var yeoman = require('yeoman-generator');
 var yosay = require('yosay');
 
 var TraviJsLibraryGenerator = yeoman.generators.Base.extend({
-  _grunt: function () {
-    this.copy('grunt/_Gruntfile.js', 'Gruntfile.js');
-    this.copy('grunt/_aliases.yml', 'grunt/aliases.yml');
-    this.copy('grunt/_jslint.yml', 'grunt/jslint.yml');
-    this.copy('grunt/_jscs.yml', 'grunt/jscs.yml');
-  },
+    _grunt: function () {
+        this.copy('grunt/_Gruntfile.js', 'Gruntfile.js');
+        this.copy('grunt/_aliases.yml', 'grunt/aliases.yml');
+        this.copy('grunt/_jslint.yml', 'grunt/jslint.yml');
+        this.copy('grunt/_jscs.yml', 'grunt/jscs.yml');
+    },
 
-  _git: function () {
-    this.copy('git/gitattributes', '.gitattributes');
-    this.copy('git/gitignore', '.gitignore');
-  },
+    _git: function () {
+        this.copy('git/gitattributes', '.gitattributes');
+        this.copy('git/gitignore', '.gitignore');
+    },
 
-  initializing: function () {
-    this.appname = this.appname.replace(/\s+/g, '-');
-  },
+    initializing: function () {
+        this.appname = this.appname.replace(/\s+/g, '-');
+    },
 
-  prompting: function () {
-    var done = this.async();
+    prompting: function () {
+        var done = this.async();
 
-    // Have Yeoman greet the user.
-    this.log(yosay('Welcome to the marvelous TraviJsLibrary generator!'));
+        // Have Yeoman greet the user.
+        this.log(yosay('Welcome to the marvelous TraviJsLibrary generator!'));
 
-    var prompts = [{
-      name: 'projectName',
-      message: 'What is the name of this project?',
-      default: this.appname
-    }];
+        var prompts = [{
+            name: 'projectName',
+            message: 'What is the name of this project?',
+            default: this.appname
+        }];
 
-    this.prompt(prompts, function (props) {
-      this.projectName = props.projectName;
+        this.prompt(prompts, function (props) {
+            this.projectName = props.projectName;
 
-      done();
-    }.bind(this));
-  },
+            done();
+        }.bind(this));
+    },
 
-  app: function () {
-    this.template('_package.json', 'package.json');
-    this._grunt();
-  },
+    app: function () {
+        this.template('_package.json', 'package.json');
+        this._grunt();
+    },
 
-  configuring: function () {
-    this.copy('editorconfig', '.editorconfig');
-    this._git();
-  },
+    configuring: function () {
+        this.copy('editorconfig', '.editorconfig');
+        this._git();
+    },
 
-  install: function () {
-    this.npmInstall([
-      'grunt',
-      'load-grunt-config',
-      'time-grunt'
-    ], {'saveDev': true});
+    install: function () {
+        this.npmInstall([
+            'grunt',
+            'load-grunt-config',
+            'time-grunt'
+        ], {'saveDev': true});
 
-    this.installDependencies({
-      npm: true,
-      bower: false,
-      skipInstall: false,
-      skipMessage: false,
-      callback: function () {
-        this.log(yosay('dependency installation complete'));
-      }.bind(this)
-    });
-  }
+        this.installDependencies({
+            npm: true,
+            bower: false,
+            skipInstall: false,
+            skipMessage: false,
+            callback: function () {
+                this.log(yosay('dependency installation complete'));
+            }.bind(this)
+        });
+    }
 });
 
 module.exports = TraviJsLibraryGenerator;
