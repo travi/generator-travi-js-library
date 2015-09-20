69ba842076b36c79f59aeff1297b8a121b3c83fc
Matt Travi <programmer@travi.org>
2015-09-20 13:05:10 -0500

    defined the default grunt task

--- test/integration/features/build.feature
+++ test/integration/features/build.feature
@@ -3,3 +3,4 @@ Feature: Grunt Build
   Scenario: gates
     When the generator is run
     Then the gates task is configured to lint
+    And the gates task is configured to be the default task
--- test/integration/features/step_definitions/build.js
+++ test/integration/features/step_definitions/build.js
@@ -17,4 +17,14 @@ module.exports = function () {
             callback();
         });
     });
+
+    this.Then(/^the gates task is configured to be the default task$/, function (callback) {
+        fs.readFile(path.join(tempDir, 'grunt/aliases.yml'), 'utf8', function (err, content) {
+            var tasks = yaml.safeLoad(content);
+
+            assert.equal(tasks.default, 'gates');
+
+            callback();
+        });
+    });
 };
