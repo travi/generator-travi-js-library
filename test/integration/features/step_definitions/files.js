738ebe5e4b9c3bd9b82275bd1d0cc371c14b6915
Matt Travi <programmer@travi.org>
2015-09-19 23:36:22 -0500

    reformatted to increase the indentation

--- test/integration/features/step_definitions/files.js
+++ test/integration/features/step_definitions/files.js
@@ -11,84 +11,85 @@ require('setup-referee-sinon/globals');
 var tempDir = path.join(__dirname, 'temp');
 
 module.exports = function () {
-  var answers = {};
+    var answers = {};
 
-  this.After(function (callback) {
-    answers = {};
+    this.After(function (callback) {
+        answers = {};
 
-    callback();
-  });
-
-  this.Given(/^the project\-name prompt is populated with "([^"]*)"$/, function (projectName, callback) {
-    answers.projectName = projectName;
-
-    callback();
-  });
-
-  this.When(/^the generator is run$/, function (callback) {
-    helpers.run(path.join( __dirname, '../../../../app'))
-      .inDir(tempDir)
-      .withPrompts(answers)
-      .withOptions({skipInstall: false})
-      .on('end', callback);
-  });
-
-  this.Then(/^the core files should be present$/, function (callback) {
-      assert.file([
-        '.gitattributes',
-        '.gitignore',
-        '.editorconfig',
-        'package.json',
-        'Gruntfile.js',
-        'grunt/aliases.yml',
-        'grunt/jslint.yml',
-        'grunt/jscs.yml'
-      ]);
-
-      assert.fileContent('.gitattributes', /^\* text=auto\n$/);
-
-      callback();
-  });
-
-  this.Then(/^the core dependencies will be installed$/, function (callback) {
-    fs.readFile(path.join(tempDir, 'package.json'), 'utf8', function (err, content) {
-      var devDeps = JSON.parse(content).devDependencies,
-          gruntSpy = sinon.spy(),
-          timeGruntSpy = sinon.spy(),
-          loadGruntConfigSpy = sinon.spy();
-
-      assert(_.has(devDeps, 'grunt'));
-      assert(_.has(devDeps, 'load-grunt-config'));
-      assert(_.has(devDeps, 'time-grunt'));
-
-      proxyquire('./temp/Gruntfile.js', {
-        'time-grunt': timeGruntSpy,
-        'load-grunt-config': loadGruntConfigSpy
-      });
-
-      require('./temp/Gruntfile.js')(gruntSpy);
-
-      sinon.assert.calledWith(timeGruntSpy, gruntSpy);
-      sinon.assert.calledWith(loadGruntConfigSpy, gruntSpy);
-
-      callback();
+        callback();
     });
-  });
 
+    this.Given(/^the project\-name prompt is populated with "([^"]*)"$/, function (projectName, callback) {
+        answers.projectName = projectName;
 
-  this.Then(/^the project\-name of "([^"]*)" is defined in the generated files$/, function (projectName, callback) {
-    fs.readFile(path.join(tempDir, 'package.json'), 'utf8', function (err, content) {
-      assert.equal(projectName, JSON.parse(content).name);
-
-      callback();
+        callback();
     });
-  });
 
-  this.Then(/^git should ignore certain files by default$/, function (callback) {
-    fs.readFile(path.join(tempDir, '.gitignore'), 'utf8', function (err, content) {
-      assert.equal('node_modules/\n', content);
-
-      callback();
+    this.When(/^the generator is run$/, function (callback) {
+        helpers.run(path.join(__dirname, '../../../../app'))
+            .inDir(tempDir)
+            .withPrompts(answers)
+            .withOptions({skipInstall: false})
+            .on('end', callback);
     });
-  });
+
+    this.Then(/^the core files should be present$/, function (callback) {
+        assert.file([
+            '.gitattributes',
+            '.gitignore',
+            '.editorconfig',
+            'package.json',
+            'Gruntfile.js',
+            'grunt/aliases.yml',
+            'grunt/jslint.yml',
+            'grunt/jscs.yml'
+        ]);
+
+        assert.fileContent('.gitattributes', /^\* text=auto\n$/);
+
+        callback();
+    });
+
+    this.Then(/^the core dependencies will be installed$/, function (callback) {
+        fs.readFile(path.join(tempDir, 'package.json'), 'utf8', function (err, content) {
+            var devDeps = JSON.parse(content).devDependencies,
+                gruntSpy = sinon.spy(),
+                timeGruntSpy = sinon.spy(),
+                loadGruntConfigSpy = sinon.spy();
+
+            assert(_.has(devDeps, 'grunt'));
+            assert(_.has(devDeps, 'load-grunt-config'));
+            assert(_.has(devDeps, 'time-grunt'));
+
+            proxyquire('./temp/Gruntfile.js', {
+                'time-grunt': timeGruntSpy,
+                'load-grunt-config': loadGruntConfigSpy
+            });
+
+            require('./temp/Gruntfile.js')(gruntSpy);
+
+            sinon.assert.calledWith(timeGruntSpy, gruntSpy);
+            sinon.assert.calledWith(loadGruntConfigSpy, gruntSpy);
+
+            callback();
+        });
+    });
+
+
+    this.Then(/^the project\-name of "([^"]*)" is defined in the generated files$/, function (projectName, callback) {
+        fs.readFile(path.join(tempDir, 'package.json'), 'utf8', function (err, content) {
+            assert.equal(projectName, JSON.parse(content).name);
+
+            callback();
+        });
+    });
+
+
+    this.Then(/^git should ignore certain files by default$/, function (callback) {
+        fs.readFile(path.join(tempDir, '.gitignore'), 'utf8', function (err, content) {
+            assert.equal('node_modules/\n', content);
+
+            callback();
+        });
+    });
 };
